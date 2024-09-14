aee5f2230f24e2c3d0a9fb020ebeac5362f2735b
ccie-trader <tech.feri02@gmail.com>
2024-09-15 00:51:55 +0300

    Back to Zero

--- cmdrunner.py
+++ cmdrunner.py
@@ -1,8 +1,2 @@
 #!/user/bin/python3
 
-import netmiko
-import json
-
-connection = netmiko.ConnectHandler(ip='192.168.254.241', device_type='cisco_ios', username='admin', password='python')
-
-print(connection.send_command('show ip int brief'))
