e92ba1299918c3b8f25ebc7da042ebb4f23b3254
ccie-trader <tech.feri02@gmail.com>
2024-09-15 04:18:21 +0800

    Start of Connecting to Multiple Device

--- cmdrunner.py
+++ cmdrunner.py
@@ -6,28 +6,9 @@ import json
 import netmiko.ssh_auth
 
 # Basic Connection
-#connection = netmiko.ConnectHandler(ip='192.168.254.241', device_type='cisco_ios', username='nde', password='automate')
+connection = netmiko.ConnectHandler(ip='192.168.254.241', device_type='cisco_ios', username='nde', password='automate')
 
-# Using a for loop
-devices = '''
-192.168.254.241
-192.168.254.242            
-'''.strip().split()
+print(connection.send_command('show ip int brief'))
 
-for device in devices:
-    try:
-        print('~'*79)
-        print("Connecting to device", device)
-        connection = netmiko.ConnectHandler(ip = device,
-                                            device_type = "cisco_ios",
-                                            username = "nde",
-                                            password = "automate")
-
-
-        print(connection.send_command('show ip int brief'))
-
-        connection.disconnect()
-    except netmiko.ssh_auth.NetMikoAuthenticationException:
-        print("Authentication Error")
 
 
