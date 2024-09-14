426c581b7ffc4fc75587811366cb848f890ee4e7
ccie-trader <63536054+ccie-trader@users.noreply.github.com>
2024-09-15 07:35:37 +0800

    Add files via upload

---
+++ get_pass_cmdrunner.py
@@ -0,0 +1,37 @@
+#!/user/bin/python3
+#USING GET PASS
+
+import netmiko
+import json
+import netmiko.ssh_auth
+
+def get_input(prompt=''):
+    try:
+        line = raw_input(prompt)
+    except NameError:
+        line = input(prompt)
+
+##USING A JSON FILE FOR DEVICES
+
+netmiko_exceptions = (netmiko.NetMikoTimeoutException,
+                      netmiko.NetMikoAuthenticationException,
+                      netmiko.NetmikoBaseException)
+
+username = get_input('Enter Username')
+password = get_input('Enter Password')
+
+with open('devices.json') as dev_file:
+    devices = json.load(dev_file)
+
+
+for device in devices:
+    try:
+        print('~'*79)
+        print("Connecting to device", device['ip'])
+        connection = netmiko.ConnectHandler(**device)
+        print(connection.send_command('show ip int brief'))
+
+        connection.disconnect()
+    except netmiko_exceptions as e:
+        print("Failed to ", device['ip'], e)
+    
