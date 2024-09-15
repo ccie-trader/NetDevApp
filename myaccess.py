c36e220d7110d0ce17fe66cdaa33d76d98506083
ccie-trader <63536054+ccie-trader@users.noreply.github.com>
2024-09-15 08:01:27 +0800

    Add files via upload

---
+++ myaccess.py
@@ -0,0 +1,23 @@
+from getpass import getpass
+
+def get_input(prompt=''):
+    try:
+        line = raw_input(prompt)
+    except NameError:
+        line = input(prompt)
+
+def get_credentials():
+    '''Prompts for, and returns, a username and passowrd'''
+
+    username = get_input('Enter Username: ')
+    password = None
+
+    while not password:
+        password = getpass()
+        password_verify = getpass('Confirm password: ')
+        
+        if password != password_verify:
+            print('Password do not match. Try again.')
+            password = None
+    return username, password
+
